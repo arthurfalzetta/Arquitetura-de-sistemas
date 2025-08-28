49f50bb785a9e70386d71e3868db02a8064865a1
arthurfalzetta <arthur.falzetta@gmail.com>
2025-08-28 12:40:29 -0300

    Update index.js
    Update index.js nas pastas lembretes e observacoes

---
+++ mss/observacoes/index.js
@@ -0,0 +1,27 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+app.use(bodyParser.json())
+
+const observacoesPorLembreteId = {}
+const { v4: uuidv4 } = require('uuid');
+
+app.post('/lembretes/:id/observacoes', (req, res) => {
+    const idObs = uuidv4();
+    const { texto } = req.body;
+    //req.params dá acesso à lista de parâmetros da URL
+    const observacoesDoLembrete =
+    observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({ id: idObs, texto });
+    observacoesPorLembreteId[req.params.id] =
+    observacoesDoLembrete;
+    res.status(201).send(observacoesDoLembrete);
+})
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.send(observacoesPorLembreteId[req.params.id] || [])
+});
+
+app.listen(5000, () => {
+    console.log('Lembretes na porta 5000')
+})
